70053ba99c57f65ca5bcf1ea125eae7d3b5be241
Cristian Badoi <cristian.badoi98@gmail.com>
2019-11-18 17:21:32 +0000

    "small" styling fixes
    I meant what i said earlier, get to work, we have less than 24 hours left

---
+++ public/js/precursorselect.js
@@ -0,0 +1,11 @@
+var precursorslist = document.getElementById("precursors-list");
+var pl_toggle = [];
+
+for (var i = 0; i < precursorslist.children.length; ++i) {
+    precursorslist.children.item(i).addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
+}
+
+var l = precursorslist.children;
+console.log(l);
