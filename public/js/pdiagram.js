c6a7846592476e354bb30def0b00721e2721877b
Cristian Badoi <cristian.badoi98@gmail.com>
2019-11-19 10:03:53 +0000

    smalll testing data added to pdiagram.js

--- public/js/pdiagram.js
+++ public/js/pdiagram.js
@@ -1,18 +1,19 @@
 var rawData = [
-	{A:75,B:25,C:0,label:'point 1'},
-	{A:73.33,B:10,C:20,label:'point 2'},
-	{A:75,B:20,C:5,label:'point 3'},
-	{A:5,B:60,C:35,label:'point 4'},
+	{A:75,B:25,C:0, label:'point 1'},
+	{A:73,B:10,C:20,label:'point 2'},
+	{A:75,B:20,C:5, label:'point 3'},
+	{A:5, B:60,C:35,label:'point 4'},
 	{A:10,B:80,C:10,label:'point 5'},
-	{A:10,B:90,C:0,label:'point 6'},
-	{A:20,B:70.5,C:10,label:'point 7'},
+	{A:10,B:90,C:0, label:'point 6'},
+	{A:20,B:70,C:10,label:'point 7'},
 	{A:10,B:20,C:70,label:'point 8'},
-	{A:15,B:5,C:80,label:'point 9'},
+	{A:15,B:5, C:80,label:'point 9'},
 	{A:10,B:10,C:80,label:'point 10'},
 	{A:20,B:10,C:70,label:'point 11'},
 ];
 
-Plotly.plot('diagram', [{
+var trace1 = {
+	name: 'known',
 	type: 'scatterternary',
 	mode: 'markers',
 	a: rawData.map(function(d) { return d.A; }),
@@ -25,8 +26,28 @@ Plotly.plot('diagram', [{
 		size: 14,
 		line: { width: 2 }
 	},
-}], {
-	ternary: {
+};
+
+var trace2 = {
+	name: 'calculated',
+	type: 'scatterternary',
+	mode: 'markers',
+	a: rawData.map(function(d) { return d.A; }),
+	b: rawData.map(function(d) { return d.B; }),
+	c: rawData.map(function(d) { return d.C; }),
+	text: rawData.map(function(d) { return d.label; }),
+	marker: {
+		symbol: 100,
+		color: '#DB7365',
+		size: 14,
+		line: { width: 2 }
+	},
+};
+
+var data = [trace1, trace2];
+var layout = {
+	ternary: 
+	{
 		sum: 100,
 		aaxis: makeAxis('A', 0),
 		baxis: makeAxis('<br>B', 45),
@@ -34,8 +55,11 @@ Plotly.plot('diagram', [{
 		bgcolor: 'rgba(0,0,0,0)'
 	},
 	paper_bgcolor: 'rgba(0,0,0,0)',
-	showlegend: true
-}, {
+	showlegend: true,
+	//hovermode: !1
+};
+
+Plotly.plot('diagram', data, layout, {
 	displayModeBar: false
 });
 
