356c3ad7b79032506ec559c9cbb38afe02af70e4
Cristian Badoi <cristian.badoi98@gmail.com>
2019-11-19 00:32:20 +0000

    minor fixes

--- routes/periodicTable.js
+++ routes/periodicTable.js
@@ -5,12 +5,18 @@ var router = express.Router();
 router.get('/', function(req,res){
 	// TODO: Authentication for users either from database or passphrase
 	res.render('periodicTable', {
-		"precursors" : [],
+		"precursors" : [
+			
+		],
 		"points" : [
-			['0.25', '0.25', '0.50'], 
-			['0.333', '0.167', '0.50'], 
-			['0.40', '0.60', '0.00']],
-		"diagram" : []
+			['id', 'formula', 'mass'],
+			['00', 'LiAlO2', '65.9478'], 
+			['01', 'Al7O3', '236.869'], 
+			['02', 'Li2Al3O2', '126.878']
+		],
+		"diagram" : [
+
+		]
 	});
 });
 
