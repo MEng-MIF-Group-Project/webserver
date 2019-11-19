0d2012d71e4436dacc79d8078753223075d26bb9
Cristian Badoi <cristian.badoi98@gmail.com>
2019-11-19 13:56:07 +0000

    added margin as option

--- public/js/menubar.js
+++ public/js/menubar.js
@@ -1,7 +1,15 @@
-var slider = document.getElementById("sample-range");
-var output = document.getElementById("sample-range-value");
-output.innerHTML = slider.value;
+var slider1 = document.getElementById("margin-range");
+var output1 = document.getElementById("margin-range-value");
+output1.innerHTML = slider1.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("sample-range");
+var output2 = document.getElementById("sample-range-value");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
 }
