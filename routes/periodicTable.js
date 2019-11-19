cd09f05a53183a950e4402f5c088623a458fb2fc
Aaron Butterworth <aaron.david.butterworth@gmail.com>
2019-11-19 13:42:15 +0000

    Menu bar changes

--- routes/periodicTable.js
+++ routes/periodicTable.js
@@ -14,19 +14,6 @@ router.get('/', function(req,res){
 			{Name: "SnS2"},
 			{Name: "LiCl"}
 		],
-		diagram: [
-			{A:75,B:25,C:0, label:'1'},
-			{A:73,B:10,C:20,label:'2'},
-			{A:75,B:20,C:5, label:'3'},
-			{A:5, B:60,C:35,label:'4'},
-			{A:10,B:80,C:10,label:'5'},
-			{A:10,B:90,C:0, label:'6'},
-			{A:20,B:70,C:10,label:'7'},
-			{A:10,B:20,C:70,label:'8'},
-			{A:15,B:5, C:80,label:'9'},
-			{A:10,B:10,C:80,label:'10'},
-			{A:20,B:10,C:70,label:'11'},
-		]
 	});
 });
 
